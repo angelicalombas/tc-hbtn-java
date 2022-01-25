a22830a7d658114f5617571f846142a41f122e62
angelicalombas <angelica_lombas@hotmail.com>
2022-01-25 17:50:24 -0300

    exercicio 5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -3,6 +3,6 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf("Valor: " + String.format("R$ %,.3f",valor+0.001) + "\nTaxa: %.2f%%\n",taxa);
+        System.out.printf("Valor: " + String.format("R$ %,.2f",valor) + "\nTaxa: %.2f%%\n",taxa);
     }
 }
