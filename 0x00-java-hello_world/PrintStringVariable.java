29e9dd65a990dcd20fd54679de4798f75d06c206
angelicalombas <angelica_lombas@hotmail.com>
2022-01-25 16:54:20 -0300

    exercicio 3

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -2,5 +2,5 @@ import static jdk.nashorn.internal.objects.NativeString.substring;
 public class PrintStringVariable {
     public static void main(String[] args) {
         String texto = "Holberton School";
-        for (int i = 0;i<2;i++){System.out.println(texto);} System.out.println(substring(texto,0,9));}
+        System.out.println(texto +"\n"+ texto +"\n"+ substring(texto,0,9));}
 }
