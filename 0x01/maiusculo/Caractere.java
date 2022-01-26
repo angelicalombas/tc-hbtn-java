011833517cac73942a38ebb226343823c5dd3807
angelicalombas <angelica_lombas@hotmail.com>
2022-01-26 11:36:52 -0300

    exercicio 2

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,12 @@
+public class Caractere {
+    public static void main(String[] args) {
+
+    }
+    public static boolean EhMaiusculo(char c){
+        if ( c >= 65 && c <= 90 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
