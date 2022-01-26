75e0ae0cca707e7e675d16ceb7c65e8574aef623
angelicalombas <angelica_lombas@hotmail.com>
2022-01-26 10:45:47 -0300

    exercicio 0

---
+++ 0x01numeros/Numeros0a99.java
@@ -0,0 +1,11 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        for (int i=0; i <= 99; i++){
+            if (i==99){
+                System.out.printf(i+"\n");
+            }else{
+                System.out.printf(i+", ");
+            }
+        }
+    }
+}
