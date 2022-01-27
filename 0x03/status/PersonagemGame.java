fa2233ae0a878418a82a397f861bd9a9ce14345a
angelicalombas <angelica_lombas@hotmail.com>
2022-01-27 16:20:53 -0300

    exercicio 3

---
+++ 0x03/status/PersonagemGame.java
@@ -0,0 +1,46 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (saudeAtual > 0){
+            status = "vivo";
+        }else{
+            status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+        if ((saudeAtual - quantidadeDeDano) < 0){
+            setSaudeAtual(0);
+        }else {
+            setSaudeAtual((saudeAtual - quantidadeDeDano));
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        if (saudeAtual + quantidadeDeCura > 100){
+            setSaudeAtual(100);
+        }else {
+            setSaudeAtual((saudeAtual + quantidadeDeCura));
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
