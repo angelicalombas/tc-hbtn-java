54de234f125a5ce9461907a62f4088ebfeac63aa
angelicalombas <angelica_lombas@hotmail.com>
2022-01-27 10:15:15 -0300

    exercicio 1

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -2,8 +2,9 @@ public class Quadrado {
 
     public static double area(double lado) throws Exception {
         if (lado < 0) {
-            throw new Exception("Lado deve possuir valor positivo");
+            throw new java.lang.IllegalArgumentException ("Lado deve possuir valor positivo");
         }
         return lado * lado;
     }
 }
+
