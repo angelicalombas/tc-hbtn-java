f5fc0c99ae3ab960573261ef3b494048510dcd2e
angelicalombas <angelica_lombas@hotmail.com>
2022-02-14 08:09:06 -0300

    exercicio 0

---
+++ 0x14/analisador_frase/AnalisadorFrase.java
@@ -0,0 +1,24 @@
+import java.util.TreeMap;
+
+public class AnalisadorFrase {
+
+    public static TreeMap contagemPalavras(String frase) {
+        frase = frase.toLowerCase();
+        frase = frase.replace("!","");
+        frase = frase.replace("?","");
+        frase = frase.replace(".","");
+
+        TreeMap<String, Integer> contagem = new TreeMap<String, Integer>();
+        String[] palavras = frase.split("\\s");
+
+        for (String p : palavras) {
+            if (contagem.containsKey(p)) {
+                contagem.put (p, contagem.get(p) + 1);
+            } else {
+                contagem.put (p, 1);
+            }
+        }
+
+        return contagem;
+    }
+}
