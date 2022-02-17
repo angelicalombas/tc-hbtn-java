8782266ab35315bc759f89cea657f42ff4315ab5
angelicalombas <angelica_lombas@hotmail.com>
2022-02-17 13:34:30 -0300

    exercicio 0

---
+++ 0x16/liquido/FolhaPagamento.java
@@ -0,0 +1,7 @@
+public class FolhaPagamento {
+
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculoSalario) {
+        double salario = funcionario.getSalario();
+        return calculoSalario.calcular(salario);
+    }
+}
