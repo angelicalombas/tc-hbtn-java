a620b5273e3a8518145cf170e241f731bd679c1d
angelicalombas <angelica_lombas@hotmail.com>
2022-02-22 11:52:44 -0300

    exercicio 1

---
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(e -> e.getCargo()));
+
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas){
+
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+
+        //return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo),Collectors.mapping(Pessoa::getIdade, Collectors.toList()));
+    }
+}
