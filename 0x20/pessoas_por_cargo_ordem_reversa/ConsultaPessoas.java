50f842f23bbb6f663d519574ef791fe22f16f36a
angelicalombas <angelica_lombas@hotmail.com>
2022-02-23 13:42:16 -0300

    exercicio 0

---
+++ 0x20/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(e -> e.getCargo()));
+
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+    }
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.filtering(pessoa -> pessoa.getIdade() > 40, Collectors.toList())));
+    }
+
+    static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(
+                Collectors.groupingBy(Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)));
+    }
+
+}
