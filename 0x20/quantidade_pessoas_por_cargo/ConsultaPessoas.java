b7e37c78612267fea13222c0767b1c5b44ff11de
angelicalombas <angelica_lombas@hotmail.com>
2022-02-23 14:46:38 -0300

    exercicio 1

---
+++ 0x20/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(e -> e.getCargo()));
+
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+    }
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.filtering(pessoa -> pessoa.getIdade() > 40, Collectors.toList())));
+    }
+
+    static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(
+                Collectors.groupingBy(Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new))
+        );
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas){
+        return pessoas.stream()
+                        .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+}
