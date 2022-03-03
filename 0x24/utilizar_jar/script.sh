9740b81ab04ffd8ea81b5445ff1bbad7be13b2b9
angelicalombas <angelica_lombas@hotmail.com>
2022-03-03 15:57:19 -0300

    exercicio 1

--- 0x24/utilizar_jar/script.sh
+++ 0x24/utilizar_jar/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 javac -cp lib/Matematica.jar com/holbertonschool/main/Program.java
-java -cp lib/Matematica.jar:. com.holbertonschool.main.Program
+java -cp lib/Matematica.jar:. com/holbertonschool/main/Program
