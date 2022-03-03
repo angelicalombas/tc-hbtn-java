4c24657615ccf8699fae1dda87f54c1dc20d1bd0
angelicalombas <angelica_lombas@hotmail.com>
2022-03-03 15:45:48 -0300

    exercicio 1

---
+++ 0x24/utilizar_jar/com/holbertonschool/matematica/Aritmeticos.java
@@ -0,0 +1,23 @@
+public class Aritmeticos {
+
+    public static int somar(int valor1, int valor2){
+      int soma =  valor1 + valor2;
+      return soma;
+    }
+
+    public static int subtrair(int valor1, int valor2){
+        int subtracao = valor1 - valor2;
+        return  subtracao;
+    }
+
+    public static int multiplicar(int valor1, int valor2){
+        int multiplicacao = valor1 * valor2;
+        return multiplicacao;
+    }
+
+    public static int dividir(int valor1, int valor2){
+        int dividir = valor1/valor2;
+        return dividir;
+    }
+
+}
